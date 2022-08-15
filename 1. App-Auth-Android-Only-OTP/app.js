491f8c70975b47d2dbf1dde6fbb3edd50fe99227
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-08-15 17:59:26 +0700

    first commit

--- 1. App-Auth-Android-Only-OTP/app.js
+++ 1. App-Auth-Android-Only-OTP/app.js
@@ -3,13 +3,13 @@
 var firebaseConfig = {
 
     apiKey: "AIzaSyCjC-USwu7ZHeq6go-Ib7SeTO_smsThV74",
-  authDomain: "blabz-app.firebaseapp.com",
-  databaseURL: "https://blabz-app-default-rtdb.firebaseio.com",
-  projectId: "blabz-app",
-  storageBucket: "blabz-app.appspot.com",
-  messagingSenderId: "187903251501",
-  appId: "1:187903251501:web:c630f4a6ba9af865f2d4a4",
-  measurementId: "G-KBG57QHP36",
+    authDomain: "blabz-app.firebaseapp.com",
+    databaseURL: "https://blabz-app-default-rtdb.firebaseio.com",
+    projectId: "blabz-app",
+    storageBucket: "blabz-app.appspot.com",
+    messagingSenderId: "187903251501",
+    appId: "1:187903251501:web:c630f4a6ba9af865f2d4a4",
+    measurementId: "G-KBG57QHP36",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
