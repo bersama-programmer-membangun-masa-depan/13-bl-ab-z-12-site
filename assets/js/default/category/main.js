71c1eeee8ce50b82f755331c67328d20af43e4ec
cosmetics-id-site <glamarycosmetics@gmail.com>
2022-09-23 23:10:55 +0700

    first commit

--- assets/js/default/category/main.js
+++ assets/js/default/category/main.js
@@ -3,9 +3,9 @@ $(function () {
     bar += '<div class="sidebar-item categories">';
         bar += '<h3 class="sidebar-title">Category</h3>';
         bar += '<ul class="mt-3">';
-            bar += '<li><a href="/m/category/popular/">Popular <span>(6)</span></a></li>';
+            bar += '<li><a href="/m/category/popular/">Popular <span>(7)</span></a></li>';
             bar += '<li><a href="/m/category/3d/">3D <span>(0)</span></a></li>';
-            bar += '<li><a href="/m/category/education/">Education <span>(1)</span></a></li>';
+            bar += '<li><a href="/m/category/education/">Education <span>(2)</span></a></li>';
             bar += '<li><a href="/m/category/business/">Business <span>(0)</span></a></li>';
             bar += '<li><a href="/m/category/post-instagram/">Post Instagram <span>(2)</span></a></li>';
         bar += '</ul>';
